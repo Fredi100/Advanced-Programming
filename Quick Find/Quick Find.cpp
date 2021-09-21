e076d1183b3fa0d47cb6fbbfec0157b3457528d1
Alfred Emsenhuber <fredi.emsenhuber@live.at>
2021-09-21 18:22:04 +0200

    Implemented Quick Find

---
+++ Quick Find/Quick Find.cpp
@@ -0,0 +1,44 @@
+// Quick Find.cpp
+// Union Find Page 14
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int p, q, id[10], N = 10;
+
+    for (int i = 0; i < N; i++)
+        id[i] = i;
+
+    while (1) {
+
+        cin >> p;
+        if (!cin)
+            break;
+        cin >> q;
+        if (!cin)
+            break;
+
+        int pid = id[p];
+        int qid = id[q];
+
+        if (pid == qid)
+            continue;
+
+        for (int i = 0; i < N; i++) {
+            if (id[i] == pid) {
+                id[i] = qid;
+            }
+        }
+
+        // Print the array
+        for (int i = 0; i < N; i++) {
+            cout << id[i];
+            if (i != N - 1)
+                cout << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
